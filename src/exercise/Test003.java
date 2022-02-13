693d0d3b66b0e24efcd45cf35b4ef19fbb1b5814
kyungstone <kyungstone@hanmail.net>
2022-02-13 22:23:10 +0900

    방법 추가

--- src/exercise/Test003.java
+++ src/exercise/Test003.java
@@ -22,13 +22,13 @@ public class Test003 {
 		//String params = "abcabcabd";
 		String s = "abcde";
 		String params = s;
-		for(int i=0; i<100000; i++) {
-			params += s;
-		}
+		
+		for(int i=0; i<100000; i++) { params += s; }
 		
 		Test003 t = new Test003();
 		long start = System.currentTimeMillis();
-		System.out.println("answer : " + t.solution(params));
+		//System.out.println("answer : " + t.solution(params));
+		System.out.println("answer : " + t.solution2(params));
 		long end = System.currentTimeMillis();
 		System.out.println("time : " + (end - start));
 	}
@@ -64,6 +64,36 @@ public class Test003 {
 			
 			if(temp.isEmpty()) {
 				answer = basic.length();
+				break;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public int solution2(String params) {
+		int answer = 0;
+		
+		loop :
+		for(int i=1; i<params.length()+1; i++) {
+			boolean result = false;
+			String basic = params.substring(0, i);
+			answer = basic.length();
+			//System.out.println("i : " + i);
+			//System.out.println("basic : " + basic);
+			for(int j=1; j<params.length()/i; j++) {
+				//System.out.println("j : " + j);
+				String target = params.substring(j*i, j*i+i);
+				//System.out.println("target : " + target);
+				if(basic.equals(target)) {
+					result = true;
+				} else {
+					result = false;
+					continue loop;
+				}
+			}
+			
+			if(result) {
 				break;
 			}
 		}
